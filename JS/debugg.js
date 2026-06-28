e584b0d055704df34dc63c1958798aeae807ab43
Michael <michaelhalperin2@gmail.com>
2026-06-28 14:53:47 +0300

    name

--- JS/debugg.js
+++ JS/debugg.js
@@ -1,4 +1,4 @@
-const names = ['Alice', 'israel', 'Charlie', 'David', 'Eve'];
+const names = ['Alice', 'israel', 'eli', 'David', 'Eve'];
 
 function shouldSkip(name) {
     return name === 'charlie';
